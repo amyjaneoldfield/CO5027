b5b99f9cde20ebc8c63cbfde496bb2706f014889
1404159 <1404159@chester.ac.uk>
2015-10-23 13:06:17 +0100

    Updated footer - suitable social media links and subscribe for offers search bar + button

--- CO5027/Site.Master.designer.cs
+++ CO5027/Site.Master.designer.cs
@@ -38,5 +38,14 @@ namespace CO5027 {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+        
+        /// <summary>
+        /// btnSubmit control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnSubmit;
     }
 }
