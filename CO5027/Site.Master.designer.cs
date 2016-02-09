5c17a15e348125c1403287962b858cfb42fca469
1404159 <1404159@chester.ac.uk>
2016-02-09 11:49:21 +0000

    login status works

--- CO5027/Site.Master.designer.cs
+++ CO5027/Site.Master.designer.cs
@@ -31,22 +31,22 @@ namespace CO5027 {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// lgnName control.
+        /// loginUserName control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.LoginName lgnName;
+        protected global::System.Web.UI.WebControls.LoginName loginUserName;
         
         /// <summary>
-        /// lgnStatus control.
+        /// loginStatus control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.LoginStatus lgnStatus;
+        protected global::System.Web.UI.WebControls.LoginStatus loginStatus;
         
         /// <summary>
         /// ContentPlaceHolder1 control.
